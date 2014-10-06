61dfd45ffd02de4cb36fc4473664203e2c69742f
totz <yuta.totz@gmail.com>
2014-10-06 15:50:13 +0900

    Set dammy cells

---
+++ CoffeeNote/AllViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  CoffeeNote
+//
+//  Created by totz on 10/1/14.
+//  Copyright (c) 2014 totz. All rights reserved.
+//
+
+import UIKit
+
+class AllViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+  
+  @IBOutlet weak var allTableView: UITableView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    allTableView.delegate = self
+    allTableView.dataSource = self
+    
+  }
+    
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  // セルに表示するテキスト
+  let texts = ["House Blend", "Pike Place Roast", "Light Note", "Kenya", "Ethiopia", "Espresso Blend", "Brazil"]
+  
+  // セルの行数
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return texts.count
+  }
+  
+  // セルの内容を変更
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+    
+    cell.textLabel?.text = texts[indexPath.row]
+    return cell
+  }
+
+  
+}
+
---
+++ CoffeeNote/NewViewController.swift
@@ -0,0 +1,36 @@
+//
+//  NewViewController.swift
+//  CoffeeNote
+//
+//  Created by totz on 10/1/14.
+//  Copyright (c) 2014 totz. All rights reserved.
+//
+
+import UIKit
+
+class NewViewController: UIViewController {
+  
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
